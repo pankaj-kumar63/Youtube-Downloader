80e72e58a80be18130a63aed0b13aa89a6297522
pankajvlog <64829287+pankajvlog@users.noreply.github.com>
2022-08-12 16:29:44 +0530

    Add files via upload

---
+++ YDownloader.py
@@ -0,0 +1,36 @@
+
+
+# link of the video to be downloaded
+
+
+# importing the module
+from pytube import YouTube
+
+# where to save
+SAVE_PATH = "E:/"  # to_do
+
+# link of the video to be downloaded
+link = "https://youtu.be/qLsHgPGHA0A"
+
+try:
+    # object creation using YouTube
+    # which was imported in the beginning
+    yt = YouTube(link)
+except:
+    print("Connection Error")  # to handle exception
+
+# filters out all the files with "mp4" extension
+mp4files = yt.__getattribute__('MKV')
+
+# to set the name of the file
+yt.set_filename('GeeksforGeeks Video')
+
+# get the video with the extension and
+# resolution passed in the get() function
+d_video = yt.get(mp4files[-1].extension, mp4files[-1].resolution)
+try:
+    # downloading the video
+    d_video.download(SAVE_PATH)
+except:
+    print("Some Error!")
+print('Task Completed!')
